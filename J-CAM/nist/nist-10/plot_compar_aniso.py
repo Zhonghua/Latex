1c205d497c15d9dede168653c9c139c143b673a9
Ma Zhonghua <mazhonghua83@gmail.com>
2011-01-13 13:40:23 +0800

    Updating figures of nist-7 to nist-12.

--- J-CAM/nist/nist-10/plot_compar_aniso.py
+++ J-CAM/nist/nist-10/plot_compar_aniso.py
@@ -7,20 +7,20 @@ axis('equal')
 pylab.title("Error convergence")
 pylab.xlabel("Degrees of freedom")
 pylab.ylabel("Error [%]")
-data = numpy.loadtxt("conv_dof_est_h1_aniso.dat")
+data = numpy.loadtxt("conv_dof_est_hp_anisoh.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="h-FEM (p=1, aniso)")
+loglog(x, y, "-s", label="hp-FEM")
 
 data = numpy.loadtxt("conv_dof_est_h2_aniso.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="h-FEM (p=2, aniso)")
+loglog(x, y, "-s", label="h-FEM (p=2)")
 
-data = numpy.loadtxt("conv_dof_est_hp_anisoh.dat")
+data = numpy.loadtxt("conv_dof_est_h1_aniso.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="hp-FEM (aniso h)")
+loglog(x, y, "-s", label="h-FEM (p=1)")
 
 legend()
 
@@ -31,20 +31,20 @@ axis('equal')
 pylab.title("Error convergence")
 pylab.xlabel("CPU time (s)")
 pylab.ylabel("Error [%]")
-data = numpy.loadtxt("conv_cpu_est_h1_aniso.dat")
+data = numpy.loadtxt("conv_cpu_est_hp_anisoh.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="h-FEM (p=1, aniso)")
+loglog(x, y, "-s", label="hp-FEM")
 
 data = numpy.loadtxt("conv_cpu_est_h2_aniso.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="h-FEM (p=2, aniso)")
+loglog(x, y, "-s", label="h-FEM (p=2)")
 
-data = numpy.loadtxt("conv_cpu_est_hp_anisoh.dat")
+data = numpy.loadtxt("conv_cpu_est_h1_aniso.dat")
 x = data[:, 0]
 y = data[:, 1]
-loglog(x, y, "-s", label="hp-FEM (aniso h)")
+loglog(x, y, "-s", label="h-FEM (p=1)")
 
 legend()
 
